cbba3f67402b9b6f78bb42cebe90f1942ac52717
Stephen Prayogo <stephenprayogo@gmail.com>
2016-07-19 14:29:02 +1000

    Reworked how Heroes Card is generated

--- app/components/HeroesCard.js
+++ app/components/HeroesCard.js
@@ -41,6 +41,15 @@ class HeroesCard extends Component {
         this.heroesDS = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.renderRow = this.renderRow.bind(this);
         this.getIndex = this.getIndex.bind(this);
+        this.sortByKey = this.sortByKey.bind(this);
+        this.generateProcessedArray = this.generateProcessedArray.bind(this);
+    }
+
+    sortByKey(array, key) {
+        return array.sort(function(a, b) {
+            var x  = a[key]; var y = b[key];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
     }
 
     getIndex(heroId, heroesArray) {
@@ -51,6 +60,54 @@ class HeroesCard extends Component {
         }
     }
 
+    generateProcessedArray(unprocessedHeroList) {
+        var processedHeroList = [];
+        var maxPlayed = unprocessedHeroList[0].games;
+
+        //Possible to use array foreach?
+        for(j = 0; j < unprocessedHeroList.length; j++) {
+
+            var currentUnprocessedHero = unprocessedHeroList[j];
+            // Process winrate %
+            var winrate = currentUnprocessedHero.win / currentUnprocessedHero.games;
+            var winPercentage = Math.round(winrate * 10000)/100;
+
+
+            // Process playedRate
+            var playedRate = currentUnprocessedHero.games / maxPlayed;
+
+            // Process index
+            var index = this.getIndex(currentUnprocessedHero.hero_id, heroes.result.heroes);
+
+            // Process staticUri
+            //var staticUri = getHeroImage(unprocessedHeroList[i].hero_id);
+
+            // Process lastPlayedTime
+            var lastPlayedTime = currentUnprocessedHero.last_played * 1000;
+            var now = moment();
+            var friendlyLastPlayedTime = moment.duration(now.diff(lastPlayedTime)).humanize();
+
+            // Process localized_name
+            var localizedName = heroes.result.heroes[index].localized_name;
+
+
+            var processedHero = {};
+            processedHero.winPercentage = winPercentage;
+            processedHero.playedRate = playedRate;
+            processedHero.index = index;
+            //processedHero.staticUri = staticUri;
+            processedHero.lastPlayed = friendlyLastPlayedTime
+            processedHero.heroId = currentUnprocessedHero.hero_id;
+            processedHero.games = currentUnprocessedHero.games;
+            processedHero.localizedName = localizedName;
+            processedHero.winrate = winrate;
+
+            processedHeroList[j] = processedHero
+
+        }
+        return processedHeroList;
+    }
+
     renderRow(rowData, i, j) {
         var rowContainer;
         if((parseInt(j)+1) % 2 == 0) {
@@ -58,58 +115,52 @@ class HeroesCard extends Component {
         } else {
             rowContainer = [styles.rowContainerOdd, {backgroundColor: this.props.alpha}];
         }
-        winrate = rowData.win / rowData.games;
-        winPercentage = Math.round(winrate * 10000)/100;
-        maxPlayed = this.props.heroes[0].games;
-        playedRate = rowData.games / maxPlayed
-        index = this.getIndex(rowData.hero_id, heroes.result.heroes);
-        var staticUri = getHeroImage(rowData.hero_id);
-        var lastPlayedTime = rowData.last_played * 1000;
-        var now = moment();
-        var friendlyLastPlayedTime = moment.duration(now.diff(lastPlayedTime)).humanize();
+        var staticUri = getHeroImage(rowData.heroId);
         return (
             <View style = {rowContainer}>
-                <View style = {styles.heroCell}>
-                    <View style = {styles.heroValueTextWrapper}>
-                        <View style = {styles.avatarContainer}>
-                            <Avatar image = {<Image source = {staticUri} />} size = {40} borderRadius = {20} style = {styles.heroIcon}/>
-                        </View>
-                        <Text style = {[styles.heroValueText, {color: this.props.secondLegend}]} numberOfLines = {1}>{heroes.result.heroes[index].localized_name}</Text>
-                    </View>
-                </View>
-                <View style = {styles.playedCell}>
-                    <Text style = {[styles.tableValueText, {color: this.props.secondLegend}]}>{rowData.games}</Text>
-                    <Slider disabled = {true}
-                            value = {playedRate}
-                            minimumTrackTintColor = {Colors.lose}
-                            maximumTrackTintColor = 'rgba(255, 255, 255, 0)'
-                            style = {styles.sliderContainer}
-                            thumbStyle = {styles.hiddenThumb}/>
-                </View>
-                <View style = {styles.winCell}>
-                    <Text style = {[styles.tableValueText, {color: this.props.secondLegend}]}>{winPercentage}%</Text>
-                    <Slider disabled = {true}
-                            value = {winrate}
-                            minimumTrackTintColor = {Colors.win}
-                            maximumTrackTintColor = 'rgba(255, 255, 255, 0)'
-                            style = {styles.sliderContainer}
-                            thumbStyle = {styles.hiddenThumb}/>
-                </View>
-                <View style = {styles.lastPlayedCell}>
-                    <Text style = {[styles.tableValueText, {color: this.props.secondLegend}]}>{friendlyLastPlayedTime}</Text>
-                </View>
+                <Text style = {[styles.heroValueText, {color: this.props.secondLegend}]} numberOfLines = {1}>{rowData.localizedName}</Text>
+                <View style = {[styles.inRowSeparator, {backgroundColor: this.props.secondLegend}]} />
+                <View style = {{flexDirection: 'row'}}>
 
+                    <View style = {styles.heroCell}>
+                        <View style = {styles.heroValueTextWrapper}>
+                            <View style = {styles.avatarContainer}>
+                                <Avatar image = {<Image source = {staticUri} />} size = {40} borderRadius = {20} style = {styles.heroIcon}/>
+                            </View>
+
+                        </View>
+                    </View>
+                    <View style = {styles.playedCell}>
+                        <Text style = {[styles.tableValueText, {color: this.props.secondLegend}]}>{rowData.games}</Text>
+                        <Slider disabled = {true}
+                                value = {rowData.playedRate}
+                                minimumTrackTintColor = {Colors.lose}
+                                maximumTrackTintColor = 'rgba(255, 255, 255, 0)'
+                                style = {styles.sliderContainer}
+                                thumbStyle = {styles.hiddenThumb}/>
+                    </View>
+                    <View style = {styles.winCell}>
+                        <Text style = {[styles.tableValueText, {color: this.props.secondLegend}]}>{rowData.winPercentage}%</Text>
+                        <Slider disabled = {true}
+                                value = {rowData.winrate}
+                                minimumTrackTintColor = {Colors.win}
+                                maximumTrackTintColor = 'rgba(255, 255, 255, 0)'
+                                style = {styles.sliderContainer}
+                                thumbStyle = {styles.hiddenThumb}/>
+                    </View>
+                    <View style = {styles.lastPlayedCell}>
+                        <Text style = {[styles.tableValueText, {color: this.props.secondLegend}]}>{rowData.lastPlayed}</Text>
+                    </View>
+
+                </View>
             </View>
         )
     }
 
     render() {
         if(this.props.heroes && this.props.heroes.length > 0) {
-            var trimmedHeroes = [];
             var heroesList = this.props.heroes;
-            for(i = 0; i < 50; i++) {
-                trimmedHeroes.push(heroesList[i]);
-            }
+            var processedHeroList = this.generateProcessedArray(heroesList);
             var heroesCardContainerStyle = [styles.heroesCardContainer, {backgroundColor: this.props.mod}];
             return (
                 <View style = {[styles.heroesCardContainer, {backgroundColor: this.props.mod}]}>
@@ -132,7 +183,7 @@ class HeroesCard extends Component {
                         </View>
                     </View>
                     <SGListView style = {styles.heroesListView}
-                        dataSource = {this.heroesDS.cloneWithRows(this.props.heroes)}
+                        dataSource = {this.heroesDS.cloneWithRows(processedHeroList)}
                         renderRow = {this.renderRow}
                         enableEmptySections = {true}
                         initialListSize = {120}
@@ -170,14 +221,12 @@ const baseStyles = _.extend(base.general, {
         height: 2
     },
     rowContainerEven: {
-        paddingTop: 10,
-        paddingBottom: 10,
-        flexDirection: 'row'
+        paddingTop: 5,
+        paddingBottom: 5
     },
     rowContainerOdd: {
-        paddingTop: 10,
-        paddingBottom: 10,
-        flexDirection: 'row'
+        paddingTop: 5,
+        paddingBottom: 5
     },
     tableHeaderContainer: {
         flexDirection: 'row'
@@ -237,6 +286,13 @@ const baseStyles = _.extend(base.general, {
     heroValueTextWrapper: {
         marginLeft: 5,
         marginRight: 5
+    },
+    inRowSeparator: {
+        height: 2,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 5,
+        marginBottom: 5
     }
 });
 
