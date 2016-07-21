ed2360577a15b5338bbc862f800e53bb7d306e64
Stephen Prayogo <stephenprayogo@gmail.com>
2016-07-21 15:32:43 +1000

    Added navigation to other's profile from Peers. Context ID structure needs to be changed.

--- app/actions/navigation_act.js
+++ app/actions/navigation_act.js
@@ -1,7 +1,27 @@
 export const types = {
     CHANGE_CONTEXT_ID: 'CHANGE_CONTEXT_ID',
     CHANGE_PARENT: 'CHANGE_PARENT',
-    CONSUME_HOME_TAB: 'CONSUME_HOME_TAB'
+    CONSUME_HOME_TAB: 'CONSUME_HOME_TAB',
+    PUSH_CONTEXT_ID: 'PUSH_CONTEXT_ID',
+    POP_CONTEXT_ID: 'POP_CONTEXT_ID'
+}
+
+export function pushContextId(id) {
+    return {
+        type: types.PUSH_CONTEXT_ID,
+        contextId: id
+    }
+}
+
+export function popContextId(id) {
+    return {
+        type: types.POP_CONTEXT_ID,
+        contextId: id
+    }
+}
+
+export function popContextId(id) {
+
 }
 
 export function changeContextId(id) {
