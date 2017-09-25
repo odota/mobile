27111739c1a0752ae8078e0731e8ee71cd8b58de
Medet Tleukabiluly <m.tleukabiluly@gmail.com>
2017-09-25 15:30:13 +0600

    cleanup devUtils by eslint

--- devUtils/generateAbilitiesImageGetter.js
+++ devUtils/generateAbilitiesImageGetter.js
@@ -1,27 +1,24 @@
-var abilities = require('../app/json/ability_ids.json');
-var fs = require('fs');
-var _ = require('lodash');
+const abilities = require('Json/ability_ids.json')
+const fs = require('fs')
 
-var data = "export function getAbilityImage(id) {\n\tvar staticImage;\n";
+let data = 'export function getAbilityImage(id) {\n\tvar staticImage;\n'
 
-for(var key in abilities) {
-    if(abilities.hasOwnProperty(key)) {
-        if(abilities[key].includes("special_bonus")) {
-            var newLine = "\t} else if (id == " + key + ") {\n\t\tstaticImage = require('../assets/talent_tree.png');\n"
-            data += newLine;
-        } else {
-            var newLine = "\t} else if (id == " + key + ") {\n\t\tstaticImage = require('../assets/abilities/" + abilities[key] + "_lg.png');\n"
-            data += newLine;
-        }
-    }
-}
-
-data += "\t}\n\t return staticImage;\n}"
-
-fs.writeFile("../app/utils/getAbilityImage.js", data, function(err) {
-    if(err) {
-        console.log(err);
+Object.keys(abilities).forEach(key => {
+    if (abilities[key].includes('special_bonus')) {
+        const newLine = '\t} else if (id == " + key + ") {\n\t\tstaticImage = require("../assets/talent_tree.png");\n'
+        data += newLine
     } else {
-        console.log("File overwritten successfully");
+        const newLine = '\t} else if (id == " + key + ") {\n\t\tstaticImage = require("../assets/abilities/" + abilities[key] + "_lg.png");\n'
+        data += newLine
     }
-});
+})
+
+data += '\t}\n\t return staticImage;\n}'
+
+fs.writeFile('../app/utils/getAbilityImage.js', data, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('File overwritten successfully')
+    }
+})
