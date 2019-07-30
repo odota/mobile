5cd3eece813b2b2d9fbcf502da18f5805f81dcaa
Stephen Prayogo <stephenprayogo@gmail.com>
2019-07-30 10:51:07 +1000

    Bumped iOS build number. Upgraded react-native-device-info version.

--- app/themes/Metrics.js
+++ app/themes/Metrics.js
@@ -1,39 +1,39 @@
-import { Platform, Dimensions } from 'react-native';
-import DeviceInfo from 'react-native-device-info';
+import { Platform, Dimensions } from "react-native";
+import DeviceInfo from "react-native-device-info";
 
 let width, height, deviceOS;
 
-width = Dimensions.get('window').width;
-height = Dimensions.get('window').height;
+width = Dimensions.get("window").width;
+height = Dimensions.get("window").height;
 
-if(Platform.OS == "ios") {
-  deviceOS = "ios"
+if (Platform.OS == "ios") {
+  deviceOS = "ios";
 } else {
-  deviceOS = "android"
+  deviceOS = "android";
 }
 
-const navBarHeight = (DeviceInfo.getModel() == "iPhone X") ? 44 : (Platform.OS === 'ios') ? 44 : 56;
-const statusBarHeight = (DeviceInfo.getModel() == "iPhone X") ? 44 : (Platform.OS === 'ios') ? 20 : 0;
+const navBarHeight = DeviceInfo.getModel() == "iPhone X" ? 44 : Platform.OS === "ios" ? 56 : 56;
+const statusBarHeight = DeviceInfo.hasNotch() == "iPhone X" ? 44 : Platform.OS === "ios" ? 20 : 0;
 
 const metrics = {
-    navBarHeight: navBarHeight,
-    statusBarHeight: statusBarHeight,
-    screenWidth: width,
-    screenHeight: height,
-    deviceOS: deviceOS,
+  navBarHeight: navBarHeight,
+  statusBarHeight: statusBarHeight,
+  screenWidth: width,
+  screenHeight: height,
+  deviceOS: deviceOS,
 
-    fonts: {
-      loginTitle: 18,
-      loginInputText: 16,
-      loginBtnText: 14,
-      title: 40,
-      accordionHeader: 16,
-      header: 20,
-      regular: 12,
-      medium: 14,
-      small: 11,
-      tiny: 8.5
-    }
-}
+  fonts: {
+    loginTitle: 18,
+    loginInputText: 16,
+    loginBtnText: 14,
+    title: 40,
+    accordionHeader: 16,
+    header: 20,
+    regular: 12,
+    medium: 14,
+    small: 11,
+    tiny: 8.5
+  }
+};
 
 export default metrics;
